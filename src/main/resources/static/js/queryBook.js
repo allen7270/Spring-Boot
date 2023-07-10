e1ee33fded18ac7714ceeccb1aa93f293f5c3ee5
Allen Feng <haha30932@gmail.com>
2023-07-10 15:36:43 +0800

    adjust query page

--- src/main/resources/static/js/queryBook.js
+++ src/main/resources/static/js/queryBook.js
@@ -1,26 +1,47 @@
-fetch("project/a1?curNum=1&size=10")
-    .then(response => response.json())
-    .then(data => {
-        const tableBody = document.querySelector("#tableData tbody");
+let pageSize = 5; // 初始頁面5筆
+let page = 1; // 初始第一頁
+function submitForm(value) {
+    pageSize = value;
+    fetchData(page, pageSize);
+}
 
-        // 遍历数据数组，并为每个对象创建表格行
-        data.object.forEach((item, index) => {
-            const row = document.createElement("tr");
-            const indexCell = document.createElement("td");
-            const authorCell = document.createElement("td");
-            const bookCell = document.createElement("td");
+function submitPage(value) {
+    page = value;
+    fetchData(page, pageSize);
+}
 
-            indexCell.textContent = index + 1;
-            authorCell.textContent = item.author;
-            bookCell.textContent = item.book;
+function fetchData(pageNumber, pageSize) {
+    const url = `project/a1?curNum=${pageNumber}&size=${pageSize}`;
 
-            row.appendChild(indexCell);
-            row.appendChild(authorCell);
-            row.appendChild(bookCell);
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
 
-            tableBody.appendChild(row);
+            const tableBody = document.querySelector("#tableData tbody");
+            tableBody.innerHTML = ""; // 清空表格内容
+
+            // table
+            data.object.forEach((item, index) => {
+                const row = document.createElement("tr");
+                const indexCell = document.createElement("td");
+                const authorCell = document.createElement("td");
+                const bookCell = document.createElement("td");
+
+                indexCell.textContent = index + 1;
+                authorCell.textContent = item.author;
+                bookCell.textContent = item.book;
+
+                row.appendChild(indexCell);
+                row.appendChild(authorCell);
+                row.appendChild(bookCell);
+
+                tableBody.appendChild(row);
+            });
+        })
+        .catch(error => {
+            console.error(error);
         });
-    })
-    .catch(error => {
-        console.error(error);
-    });
+}
+
+// init
+fetchData(page, pageSize);
