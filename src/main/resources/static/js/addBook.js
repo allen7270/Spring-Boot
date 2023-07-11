bfc6f39242de5bb22124537461d62d266b5fda09
Allen Feng <haha30932@gmail.com>
2023-07-11 12:00:06 +0800

    add alert success/fail

--- src/main/resources/static/js/addBook.js
+++ src/main/resources/static/js/addBook.js
@@ -14,10 +14,35 @@ function addBook() {
     xhr.open("POST", url, true);
     xhr.setRequestHeader("Content-Type", "application/json");
     xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            var response = JSON.parse(xhr.responseText);
-            console.log(response);
+        if (xhr.readyState === 4 ) {
+            if (xhr.status === 200) {
+                var response = JSON.parse(xhr.responseText);
+                console.log(response);
+                showResultMessage("書籍新增成功！", "success");
+            } else {
+                showResultMessage("書籍新增失敗！", "error");
+            }
         }
     };
     xhr.send(jsonString);
+
+    // 清空欄位
+    var inputs = document.querySelectorAll('input[type="text"]');
+    inputs.forEach(function(input) {
+        input.value = '';
+    });
+}
+
+function showResultMessage(message, type) {
+    var modalOverlay = document.getElementById("modalOverlay");
+    var modalMessage = document.getElementById("modalMessage");
+    modalMessage.innerHTML = message;
+    modalMessage.classList.remove("success", "error");
+    modalMessage.classList.add(type);
+    modalOverlay.style.display = "flex";
+}
+
+function closeModal() {
+    var modalOverlay = document.getElementById("modalOverlay");
+    modalOverlay.style.display = "none";
 }
