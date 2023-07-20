8bd545305daaaf8109fdd1b37b232a60b398406b
Allen Feng <haha30932@gmail.com>
2023-07-20 13:31:39 +0800

    add personnel page btn

--- src/main/resources/static/js/viewPersonnel.js
+++ src/main/resources/static/js/viewPersonnel.js
@@ -1,3 +1,15 @@
+let personalPageSize = 5; // 初始頁面5筆
+let personalPage = 1; // 初始第一頁
+
+function submitPersonalForm(value) {
+    personalPageSize = value;
+    viewPersonnel(personalPage, personalPageSize);
+}
+
+function submitPersonalPage(value) {
+    personalPage = value;
+    viewPersonnel(personalPage, personalPageSize);
+}
 function viewPersonnel() {
     const popupContainer = document.getElementById('popupPersonalContainer');
     const personnelTableBody = document.querySelector('#personnelTable tbody');
@@ -5,7 +17,7 @@ function viewPersonnel() {
 
     personnelTableBody.innerHTML = '';
 
-    const url = `project/role?curNum=1&size=5`;
+    const url = `project/role?curNum=${personalPage}&size=${personalPageSize}`;
 
     fetch(url)
         .then(response => response.json())
@@ -26,15 +38,15 @@ function viewPersonnel() {
 
                 personnelTableBody.appendChild(row);
 
-                // const pageData = data["page"];
-                // const totalPages = pageData.totalPages;
-                // addButtons(totalPages);
-                //
-                // const totalElements = pageData.totalElements;
-                // const curNum = pageData.curNum;
-                //
-                // const totalPageData = document.getElementById('totalPageData');
-                // totalPageData.innerText = `| 共${totalElements}筆，第${curNum}頁，共${totalPages}頁`;
+                const pageData = data["page"];
+                const totalPages = pageData.totalPages;
+                addPersonalButtons(totalPages);
+
+                const totalElements = pageData.totalElements;
+                const curNum = pageData.curNum;
+
+                const totalPagePersonalData = document.getElementById('totalPagePersonalData');
+                totalPagePersonalData.innerText = `| 共${totalElements}筆，第${curNum}頁，共${totalPages}頁`;
 
             });
         });
@@ -44,4 +56,19 @@ function viewPersonnel() {
     closeButton.addEventListener("click", function() {
         popupContainer.style.display = "none";
     });
+}
+
+function addPersonalButtons(totalPages) {
+    const paginationContainer = document.getElementById("paginationPersonalButtons");
+    paginationContainer.innerHTML = ""; // 清空容器内容
+
+    for (let i = 1; i <= totalPages; i++) {
+        const button = document.createElement("button");
+        button.textContent = i;
+        button.className = "button";
+        button.onclick = function () {
+            submitPersonalPage(i);
+        };
+        paginationContainer.appendChild(button);
+    }
 }
